12ad55718e6196e345709c305a61bee8dc5ee633
Macwel <wifi-215@yandex.ru>
2022-08-19 13:31:04 +0500

    add decoder

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,12 @@
 import fetcher from 'node-fetch';
 import AdmZip from 'adm-zip';
+import * as fs from 'fs';
+import iconv from 'iconv-lite';
+import path from 'path';
 
 export default class App {
   #lastName = '';
+  #path = path.resolve(__dirname, '../data/');
 
   constructor() {
     this.#boot();
@@ -12,18 +16,29 @@ export default class App {
     console.log(new Date(), '[Extractor]: Start');
     const res = await fetcher(process.env.PARSE_URL);
     if (res.status === 200) {
-      const zip = new AdmZip(await res.buffer());
-      zip.extractAllTo('./data', true);
-      this.#lastName = zip.getEntries()[0].entryName;
+      // check for a successful response
+      const zip = new AdmZip(await res.buffer()); // convert received response to buffer and init instance
+      zip.extractAllTo('./data', true); // extract zip to ./data folder
+      this.#lastName = zip.getEntries()[0].entryName; // get name of file in zip
     } else {
       throw new Error('[Extractor-Error]: Couldnt download data ZIP');
     }
     console.log(new Date(), '[Extractor]: Data is ready to use');
   }
 
+  async decoder(): Promise<void> {
+    console.log(new Date(), '[Decoder]: Start');
+    fs.createReadStream(`${this.#path}/${this.#lastName}`)
+      .pipe(iconv.decodeStream('win1251'))
+      .pipe(iconv.encodeStream('UTF-16'))
+      .pipe(fs.createWriteStream(`${this.#path}/decode_${this.#lastName}`));
+    console.log(new Date(), '[Decoder]: Finish decoder');
+  }
+
   async #boot() {
     console.log(new Date(), '[App]: Start');
     await this.extractor();
+    await this.decoder();
 
     console.log(new Date(), '[App]: Finish App');
   }
